bf36a1efd3f98ba486e786a2697875638738a583
Iñigo Fernández del Amo <inigofernandezdelamo@outlook.com>
2021-01-23 13:20:16 +0100

    Updated r code for creating leaflet map
    Using fontawesome library in leaflet for makeAwesomeIcons

--- week2/code/skimap.R
+++ week2/code/skimap.R
@@ -1,3 +1,26 @@
-# Download ski data available from web
+# Set libraries to be used in this assignment
+library(knitr)
+library(leaflet)
+library(fontawesome)
+library(dplyr)
+# Set public URLs for ski data and icon available from web
 skiURL <- "https://gist.githubusercontent.com/Ewiseman/b251e5eaf70ca52a4b9b10dce9e635a4/raw/9f0100fe14169a058c451380edca3bda24d3f673/ski_resort_stats.csv"
-skiData <- read.csv(url(skiURL))
+iconURL <- "https://github.com/ifernandezblanc/coursera_dss_developingdataproducts_assignments/blob/gh-pages/week2/resources/skiicon.png"
+# Download ski data omitting rows with NA values and pre-process
+skiData <- na.omit(read.csv(url(skiURL)))
+# Identify the highest percentage of slope types (green, blue or black)
+skiData <- skiData %>% 
+  mutate (slope_trend = 
+            if_else(green_percent >= blue_percent, 
+                    if_else(green_percent >= black_percent, 'green', 'black'), 
+                    if_else(blue_percent >= black_percent, 'blue', 'black')))
+# Create map using ad-hoc icons from font awesome: https://github.com/rstudio/fontawesome
+# Markers will be colored according to slope_trend
+# Markers will have as pop-up the ski resort_name
+skiMap <- leaflet(data = skiData) %>% 
+  addTiles() %>% 
+  addAwesomeMarkers(~lon, ~lat,
+                    icon = makeAwesomeIcon(icon = "skiing", library = "fa",
+                                           iconColor = "white",
+                                           markerColor = ~slope_trend),
+                    popup = ~resort_name)
